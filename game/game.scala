3e3df4cb48049bf4cf85484ec1f7655f3ae46549
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-04-20 14:09:06 +0200

    Clenaup + more descriptions

--- game/game.scala
+++ game/game.scala
@@ -11,8 +11,6 @@ import requests.{Response => _, head => _, *}
 import scalatags.Text.all.*
 import upickle.default.*
 import java.util.Base64
-import java.nio.charset.StandardCharsets
-import java.net.URLEncoder
 
 import scala.concurrent.*
 import scala.util.Random
@@ -36,14 +34,18 @@ object CaskHttpServer extends cask.MainRoutes {
   println("Starting web server on port 8080")
   @get("/")
   // Entry point - show link to login
+  // <div> click <a href=LoginUrl> here </a> to login </div>
   def entrypoint() = ???
 
+  // Get the authorization code from the Spotify
+  // Redirect to /run
   @get("/start")
   def start(code: String) = ???
 
   @get("/run")
   def run(): Response.Raw = {
     // Check if authenticated, if not show link to /login
+    // <div> Please try to <a href=loginUrl> login in </a> again to play
     // else:
     // choose random song from playlists
     // Send request to Spotify to start playing the song on device
@@ -52,6 +54,14 @@ object CaskHttpServer extends cask.MainRoutes {
     // Show form where user can insert their guess, it should contain:
     // * some clue about the cong
     // * should send the reuslt to /submit using post and encoding type 'application/x-www-form-urlencoded'
+    /* <div> Clue ... </div>
+     * <form action=/submit method=post, enctype=UrlEncoded, autocomplete=fales>
+     *  <input type=hidden name=answer value=CorrectAnswers, widhth=0%>
+     *  <input type=text, name=guess/>
+     *  <input type=submit value=Guess/>
+     * </form>
+     */
+
     ???
   }
 
@@ -66,6 +76,13 @@ object CaskHttpServer extends cask.MainRoutes {
     // Check the guess and correct answer
     // Show message wheter message is correct or not
     // Show a link to alowing to retry with different song
+    /*
+     * <h1>Result</h1>
+     * <div>
+     *   Correct answer was ...
+     *   <a href=/run> Click to try again </a>
+     * </div>
+     */
     ???
   }
 
@@ -74,12 +91,15 @@ object CaskHttpServer extends cask.MainRoutes {
 
 object Template:
   private lazy val styles =
-    tag("style")(os.read(os.resource(getClass.getClassLoader) / "styles.css"))
-  def apply(
-      title: String,
+    tag("style")(
+      os.read(os.resource(getClass.getClassLoader) / "styles.css")
+    )
+  def apply(title: String)(
       content: scalatags.generic.Modifier[scalatags.text.Builder]*
   ): scalatags.Text.all.doctype =
-    doctype("html")(html(styles, head(tag("title")(title)), body(content: _*)))
+    doctype("html")(
+      html(styles, head(tag("title")(title)), body(content: _*))
+    )
 
 private final val UrlEncoded = "application/x-www-form-urlencoded"
 object Spotify:
@@ -92,13 +112,10 @@ object Spotify:
 
   final val LoginUrl: String =
     // https://developer.spotify.com/documentation/web-api/quick-start/ #Call the Spotify Accounts Service
-    val parameters = Map(
-      "client_id" -> ClientId,
-      "response_type" -> "code",
-      "redirect_uri" -> RedirectUrl,
-      "scope" -> "user-read-playback-state user-modify-playback-state"
-    )
-    // Build URL encoded list of parameters
+    // https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+    import java.nio.charset.StandardCharsets
+    import java.net.URLEncoder
+    val scope = "user-read-playback-state user-modify-playback-state"
     val params = ???
     s"$SpotifyAuthUrl/authorize?$params"
 
@@ -116,12 +133,21 @@ object Spotify:
       val accessToken = ???
       RequestAuth.Bearer(accessToken)
   }
+
+  // https://developer.spotify.com/console/get-playlists/
+  // https://api.spotify.com/v1/users/{user_id}/playlists
   def listPlaylists(user: String): List[Playlist] = ???
 
+  // https://developer.spotify.com/console/get-playlist-tracks/
+  // https://api.spotify.com/v1/playlists/{playlist_id}/tracks
   def listPlaylistItems(playlist: Playlist): List[PlaylistItem] = ???
 
+  // https://developer.spotify.com/console/put-play/
+  // https://developer.spotify.com/console/put-play/
   def play(playlist: Playlist, offset: Int) = ???
 
+  // https://developer.spotify.com/console/put-pause/
+  // https://api.spotify.com/v1/me/player/pause
   def pause()(using auth: RequestAuth): Unit = ???
 
 case class Playlist(uri: String, id: String, name: String)
@@ -130,6 +156,7 @@ case class Track(album: Album, name: String)
 case class Album(name: String, artists: List[Artist])
 case class Artist(name: String)
 
+// Macro generated json reders
 given Reader[Album] = macroR
 given Reader[Artist] = macroR
 given Reader[Playlist] = macroR
